e7ace14c40476ca797c46dd5bae3a21c8cf36901
Alex <aleshaascet@gmail.com>
2024-11-14 19:39:23 +0300

    update version

--- helloWorld.php
+++ helloWorld.php
@@ -1,3 +1,19 @@
 <?php
     echo  "Hello World!";
+/**
+ * test Project
+ * Version: 0.0.1
+ * Description: This is a sample PHP project.
+ */
+
+// Функция для отображения информации о проекте
+function displayProjectInfo() {
+    $version = '0.0.1'; //версия
+    echo "Welcome to  My Test Project\n";
+    echo "Current version: $version\n";
+}
+
+// Вызов функции
+displayProjectInfo();
+
 ?>
